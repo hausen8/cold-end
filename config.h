23592186a14392028571ee474dba58bba804a703
Peter / Talla83 <58005118+talla83@users.noreply.github.com>
2020-09-22 19:35:10 +0200

    Add files via upload

--- config.h
+++ config.h
@@ -10,7 +10,9 @@
   Daniel Seoane / SeoGeo
 
   Rewritten by Tilman, 2020-09-08
-  Last edited: 2020-09-17
+  Rewritten by Talla83.de, 2020-09-21
+
+  Last edited: 2020-09-21
 
 */
 
@@ -29,10 +31,20 @@
 #define outSpitLED 12                       // Spit LED pin
 
 
+// New motor control !!!                    // added Talla83.de
+#include <TimerOne.h>
+// New motor control !!!                    // added Talla83.de
+
 // Mist and spit
-#define max_flow_rate 50                    // Maximum coolant flow rate (min. delay time between step LOW and HIGH)
-#define fast_flow_rate 100                  // Fast mode flow rate value
-#define spit_flow_rate 100                  // Spit mode flow rate value
+
+// max_flow_rate (max value = 1900 / min value = 500) 
+// value 1900 = 0,40 to 61 rpm (1/16 microstepping)
+// value 1000 = 0,48 to 74 rpm (1/16 microstepping)
+// value  500 = 0,96 to 148 rpm (1/16 microstepping)
+
+#define max_flow_rate  500UL                  // Maximum coolant flow rate (see table above)
+#define fast_flow_rate 80                   // Fast mode flow rate value 80= 115rpm /125=74rpm / 250=37rpm
+#define spit_flow_rate 80                   // Spit mode flow rate value 80= 115rpm /125=74rpm / 250=37rpm
 #define spit_min_time 1000                  // Spit mode minimum time in milliseconds
 #define spit_max_time 8000                  // Spit mode maximum time in milliseconds
 #define min_ml_per_hour 1                   // Minimum milliliter per hour
@@ -41,14 +53,14 @@
 
 // Operator control
 #define switch_debounce 30                  // Delay time in milliseconds to debounce switch
-// #define linear_control                      // Choose between linear and exponential flow control
+//#define linear_control                      // Choose between linear and exponential flow control
 #define smooth_filter 0.05                  // Exponential filter to smooth potentiometer values
-#define screen_delay 500                    // Minimum delay between two screen refreshes in milliseconds
+#define screen_delay 100                    // Minimum delay between two screen refreshes in milliseconds
 
 
 // Display
-// #define SSD1306                             // Uncomment for OLED with SSD1306 controller
-#define SH1106                              // Uncomment for OLED with SH1106 controller
+#define SSD1306                             // Uncomment for OLED with SSD1306 controller
+//#define SH1106                              // Uncomment for OLED with SH1106 controller
 
 #define SCREEN_WIDTH 128                    // OLED display width in pixels
 #define SCREEN_HEIGHT 64                    // OLED display height in pixels
@@ -71,8 +83,3 @@
   #include <Fonts/FreeSans18pt7b.h>
   Adafruit_SH1106 display(OLED_RESET);
 #endif
-
-
-
-
-
